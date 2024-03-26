700dde35f8b074c7ca9b67f883c78209c7e46ef0
Ginasrrhmwti <141693381+Ginasrrhmwti@users.noreply.github.com>
2024-03-26 11:22:35 +0700

    Add files via upload

---
+++ Statistik.vue
@@ -0,0 +1,46 @@
+<template> 
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script setup>
+import Chart  from 'chart.js/auto';
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Pengunjung',
+      backgroundColor: [
+        'rgb(135, 198, 119)',
+        'rgb(208, 181, 84)',
+        'rgb(183, 31, 31)',
+        'rgb(135, 198, 119)',
+        'rgb(228, 202, 107)',
+      ],
+      borderColor: 'rgb(255, 100, 132)',
+      data: [6, 8, 10, 9, 9,],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+  onMounted(()=>{
+    const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+  })
+</script>
