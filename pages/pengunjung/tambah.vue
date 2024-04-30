29eb21e051f01438337986772184bf1333910504
ginarahma <141693381+Ginasrrhmwti@users.noreply.github.com>
2024-04-30 09:06:39 +0700

    Add files via upload

--- pages/pengunjung/tambah.vue
+++ pages/pengunjung/tambah.vue
@@ -43,44 +43,6 @@
     </div>
 </template>
 
-<script setup>
-const supabase = useSupabaseClient()
-
-const members = ref([])
-const objectives = ref([])
-const form =ref({
-    nama:"",
-    keanggotaan:"",
-    tingkat:"",
-    jurusan:"",
-    kelas:"",
-    keperluan:"",
-})
-const kirimData = async() => {
-    const { error } = await supabase.from('pengunjung').insert([form.value])
-    if(!error)navigateTo('/pengunjung')
-}
-
-const getKeanggotaan = async () => {
-    const { data, error } = await supabase.from('keanggotaan').select('*')
-    if(data) members.value = data
-}
-
-const getkeperluan = async () => {
-    const { data, error } = await supabase.from('keperluan').select('*')
-    if(data) objectives.value = data
-}
-
-onMounted(() => {
-    getKeanggotaan()
-    getkeperluan()
-})
-</script>
-
-<form @submit.prevent="kirimData">
-    
-</form>
-
 <style scoped>
 .form-control {
     background-color: #D9D9D9;
